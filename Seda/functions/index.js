5b02cc043bbd9414c3ea9dd851e9a7519caac24b
Dongjie Chen <cdjchen@ucdavis.edu>
2020-05-21 22:45:03 -0700

    register an account on Stripefor the user and add the StipeId to users document field when the user register a new account

--- Seda/functions/index.js
+++ Seda/functions/index.js
@@ -1,8 +1,22 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+let db = admin.firestore();
+
+// Set your secret key. Remember to switch to your live secret key in production!
+// See your keys here: https://dashboard.stripe.com/account/apikeys
+const stripe = require('stripe')(functions.config().stripe.test_payment_service_key);
+
+exports.createStripeCustomer = functions.firestore.document('users/{username}').onCreate(async (snap, context) => {
+    const data = snap.data();
+    const email = data.username.concat('@seda.com');
+    const customer = await stripe.customers.create({email: email})
+    db.collection('users').doc(String(data.uid)).set({stripeId: customer.id }, {merge: true});
+});
+
+//exports.helloWorld = functions.https.onRequest((request, response) => {
+//    console.log('This is the console message')
+// response.send("Hello from Firebase!");
+//});
+  
